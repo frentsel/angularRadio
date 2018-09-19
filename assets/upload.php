7fe3fd4db367adf81b4306c63c35e59a17dc9380
Oleksandr Frentsel <Oleksandr.Frentsel@sophos.com>
2018-09-19 13:37:02 +0300

    Auto-generated commit

---
+++ assets/upload.php
@@ -0,0 +1,12 @@
+<?php
+
+$uploads_dir = __DIR__.'/uploads';
+
+foreach ($_FILES['file']['error'] as $key => $error) {
+
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES['file']['tmp_name'][$key];
+        $name = basename($_FILES['file']['name'][$key]);
+        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+    }
+}
