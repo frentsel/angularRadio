fab28d20c563f4ce92b373b31db452ea03207225
Oleksandr Frentsel <Oleksandr.Frentsel@sophos.com>
2018-09-20 14:45:35 +0300

    made playlist module

--- src/app/artist/artist.module.ts
+++ src/app/artist/artist.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtistComponent } from './artist.component';
 import { RouterModule, Routes } from '@angular/router';
-import { PlaylistComponent } from './playlist/playlist.component';
 import { ArtistPreviewModule } from '../components/artist-preview/artist-preview.module';
 import { SimilarModule } from '../components/similar/similar.module';
 
@@ -12,9 +11,9 @@ const routes: Routes = [{
   children: [
     { path: '', redirectTo: 'bio', pathMatch: 'full' },
     { path: 'bio', loadChildren: 'app/artist/bio/bio.module#BioModule' },
-    { path: 'playlist', component: PlaylistComponent },
+    { path: 'playlist', loadChildren: 'app/artist/playlist/playlist.module#PlaylistModule' },
     // { path: 'albums/:album', loadChildren: 'app/artist/albums.module#AlbumsModule' },
-    { path: 'video', component: PlaylistComponent },
+    // { path: 'video', component: PlaylistComponent },
     { path: 'photos', loadChildren: 'app/artist/photos/photos.module#PhotosModule' },
   ]
 }];
@@ -28,7 +27,6 @@ const routes: Routes = [{
   ],
   declarations: [
     ArtistComponent,
-    PlaylistComponent,
   ],
   exports: [
     ArtistComponent
--- src/app/artist/playlist/playlist.component.ts
+++ src/app/artist/playlist/playlist.component.ts
@@ -1,39 +1,41 @@
-import {ActivatedRoute} from '@angular/router';
-import {Component, OnInit} from '@angular/core';
-import {HttpAppService} from '../../http-app.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpAppService } from '../../http-app.service';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'playlist.component.html',
-    styleUrls: ['playlist.component.css']
+  templateUrl: 'playlist.component.html',
+  styleUrls: ['playlist.component.css']
 })
 
 export class PlaylistComponent implements OnInit {
 
-    tracks: Array<any>;
-    title: string;
+  tracks: any[];
+  title: string;
 
-    constructor(
-        private activatedRoute: ActivatedRoute,
-        private httpAppService: HttpAppService,
-    ) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpAppService: HttpAppService,
+  ) { }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        const _params = {
-            'method': 'artist.getTopTracks',
-            'autocorrect': 1,
-            'artist': this.activatedRoute.snapshot.parent.params['artist'],
-            'api_key': '02ec4e9d3a6dec29749f9d0a2cf3f598',
-            'page': 1,
-            'limit': 50,
-            'format': 'json',
-        };
+    this.activatedRoute.parent.params.subscribe(() => {
 
-        this.httpAppService.getJSON(`http://ws.audioscrobbler.com/2.0/`, _params)
-            .then(data => {
-                this.tracks = data.toptracks.track;
-                this.title = 'Top 50 tracks';
-            });
-    }
+      const _params = {
+        'method': 'artist.getTopTracks',
+        'autocorrect': 1,
+        'artist': window.location.pathname.split('/').slice(2, 3)[0],
+        'api_key': '02ec4e9d3a6dec29749f9d0a2cf3f598',
+        'page': 1,
+        'limit': 50,
+        'format': 'json',
+      };
+
+      this.httpAppService.getJSON(`http://ws.audioscrobbler.com/2.0/`, _params)
+        .then(data => {
+          this.tracks = data.toptracks.track;
+          this.title = 'Top 50 tracks';
+        });
+    });
+  }
 }
