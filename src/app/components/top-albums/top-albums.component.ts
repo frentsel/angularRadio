ab8c8565693dbe641e2a32af6fed51ae61be925d
Oleksandr Frentsel <Oleksandr.Frentsel@sophos.com>
2018-09-23 14:46:53 +0300

    fixed issue with topAlbums module

--- src/app/components/top-albums/top-albums.component.ts
+++ src/app/components/top-albums/top-albums.component.ts
@@ -1,46 +1,30 @@
-import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy } from '@angular/core';
-import { HttpAppService } from '../../services/http-app.service';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { LastFmService } from '../../services/lastfm.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-top-albums',
-  templateUrl: './top-albums.component.html',
   styleUrls: ['./top-albums.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: './top-albums.component.html',
 })
 
 export class TopAlbumsComponent implements OnInit, OnChanges {
 
   @Input() artist: string;
-  albums: any[];
+  public albums$: Observable<any[]>;
 
-  constructor(private httpAppService: HttpAppService) {
+  constructor(private api: LastFmService) { }
+
+  loadAlbums(): void {
+    const params = { artist: this.artist };
+    this.albums$ = this.api.getAlbums(params)
+      .pipe(
+        map((albums) => albums.slice(0, 5))
+      );
   }
 
-  filterAlbums() {
-    this.albums = this.albums.filter(album => {
-      return album.name !== '(null)' && album.image[2]['#text'];
-    }).slice(0, 5);
-  }
-
-  loadAlbums() {
-
-    const params = {
-      'method': 'artist.gettopalbums',
-      'artist': this.artist,
-      'api_key': '02ec4e9d3a6dec29749f9d0a2cf3f598',
-      'lang': 'ru',
-      'limit': 30,
-      'format': 'json'
-    };
-
-    this.httpAppService.getJSON(`https://ws.audioscrobbler.com/2.0/`, params)
-      .then(data => {
-        this.albums = data['topalbums']['album'];
-        this.filterAlbums();
-      });
-  }
-
-  ngOnInit() {
+  ngOnInit(): void {
     this.loadAlbums();
   }
 
